059d8eeac5ada16e2eb7519a591928d0bb4ae3b7
Jing Lv <jinglv@thoughtworks.com>
2015-07-28 12:31:24 +0800

    add application properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,17 @@
+package gpotes.examples.mongospring;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-@ComponentScan
+import org.springframework.context.annotation.Configuration;
+
 @EnableAutoConfiguration
-public class Main {
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Main.class);
-        ProductDetail detail = new ProductDetail();
-        detail.setProductId("ABCD1234");
-        detail.setProductName("Dan's Book of Writing");
-        detail.setShortDescription("A book about writing books.");
-        detail.setLongDescription("In this book about writing books, Dan will show you how to write a book.");
-        detail.setInventoryId("009178461");
-        ProductDetailRepository repository = ctx.getBean(ProductDetailRepository.class);
-        repository.save(detail);
-        for (ProductDetail productDetail : repository.findAll()) {
-            System.out.println(productDetail.getProductId());
-        }
+@Configuration
+@ComponentScan
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
     }
 }
